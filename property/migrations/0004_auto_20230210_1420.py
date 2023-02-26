74a106227932724bf9a1c8998ba1909509fc1698
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-02-26 18:31:06 +0300

    fix migrations

--- property/migrations/0004_auto_20230210_1420.py
+++ property/migrations/0004_auto_20230210_1420.py
@@ -8,6 +8,9 @@ def mark_new_buildings_automatically(apps, schema_editor):
     Flat.objects.\
         filter(construction_year__gte=2015).\
         update(new_building = True)
+    Flat.objects. \
+        filter(construction_year__lt=2015). \
+        update(new_building=False)
 
 
 
