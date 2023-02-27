b45334e113fab03ffaae23fde5ef706923b051bd
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-02-27 15:16:44 +0300

    change migration

--- property/migrations/0004_auto_20230210_1420.py
+++ property/migrations/0004_auto_20230210_1420.py
@@ -7,7 +7,7 @@ def mark_new_buildings_automatically(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Flat.objects.\
         filter(construction_year__gte=2015).\
-        update(new_building = True)
+        update(new_building=True)
     Flat.objects. \
         filter(construction_year__lt=2015). \
         update(new_building=False)
